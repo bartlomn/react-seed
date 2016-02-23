d4a6c398712dffe091c83d6e0b0345ecb57c6b92
BART NOWAK <bnowak@bnowak.com>
2016-02-23 09:44:07 +0000

    Fully compliant with ES7

--- src/Foo.js
+++ src/Foo.js
@@ -1,21 +1,75 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
 
 class Foo extends Component {
+
+  /**
+   * Default css classes of the component
+   * @type {String}
+   */
+  static defaultClasses = "default-local-class";
+
+  /**
+   * PropTypes ES7 way
+   * @see https://facebook.github.io/react/blog/2015/01/27/react-v0.13.0-beta-1.html#es7-property-initializers
+   */
+  static propTypes = {
+      classNames: React.PropTypes.string,
+      label: React.PropTypes.string.isRequired
+  };
+
+  /**
+   * Initial props ES7 way
+   * @see https://facebook.github.io/react/blog/2015/01/27/react-v0.13.0-beta-1.html#es7-property-initializers
+   */
+  static defaultProps = {
+      classNames: 'default-prop-class'
+  };
+
+  /**
+   * Initial state ES7 way
+   * @see https://facebook.github.io/react/blog/2015/01/27/react-v0.13.0-beta-1.html#es7-property-initializers
+   */
+  state = {
+      count: 0
+  };
+
+  /**
+   * Default constructor
+   * @param  {Object} props component props
+   * @return {void}
+   */
   constructor(props) {
     super(props);
-    this.state = {count: 0};
-    setInterval(::this.tick, 1000);
   }
 
-  tick() {
+  /**
+   * React lifecycle method
+   * @return {void}
+   */
+  componentWillMount() {
+    setInterval(this.tick, 1000);
+  }
+
+  /**
+   * Using syntacticall autobinding ES7 way.
+   * @see https://facebook.github.io/react/blog/2015/01/27/react-v0.13.0-beta-1.html#autobinding
+   */
+  tick = () => {
     this.setState({ count: this.state.count + 1 });
   }
 
+  /**
+   * React render
+   * @return {void}
+   */
   render() {
     return (
-      <div>
-        <h1>Hello, this is react.</h1>
-        Count is {this.state.count}
+      <div
+        className={ classnames( Foo.defaultClasses, this.props.classNames ) }
+        >
+          <h1>Hello, this is { this.props.label }.</h1>
+          Count is {this.state.count}
       </div>
     )
   }
--- src/app.js
+++ src/app.js
@@ -2,4 +2,4 @@ import React from 'react'
 import {render} from 'react-dom'
 import Foo from './Foo';
 
-render(<Foo/>, document.getElementById('root'));
+render(<Foo label="Dog" classNames="outer-class"/>, document.getElementById('root'));
